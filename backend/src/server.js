ec2a90c6eb5cdd19b105a397cbcc4e128e5c0051
adridder02 <u23542676@tuks.co.za>
2025-09-04 22:18:14 +0200

    Fixed a minor issue in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,32 +5,26 @@ import authRoutes from "./routes/auth.js";
 import projectRoutes from "./routes/projects.js";
 import activityRoutes from "./routes/activities.js";
 
-
-// Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// CREATE APP
 const app = express();
-
-// SERVE STATIC FILES FROM frontend/public
 app.use(express.static(path.join(__dirname, "../../frontend/public")));
 
-// Middleware
+// middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../../frontend/public")));
 
-// Routes
+// routes
 app.use("/api", authRoutes);
 app.use("/api", projectRoutes);
 app.use("/api", activityRoutes);
 
-// SERVE INDEX.HTML FOR ALL OTHER ROUTES TO SUPPORT CLIENT-SIDE ROUTING
+// for all routes
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/public", "index.html"));
 });
 
-// PORT TO LISTEN TO
 app.listen(8080, () => {
-  console.log("Listening on localhost:1337");
+  console.log("Listening on localhost");
 });
